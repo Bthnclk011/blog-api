2acd263e38c0f8d42d705e465e7978fd5bdcaaee
Batuhan <bthnclk011@gmail.com>
2024-05-27 18:37:41 +0300

    configure backend mongoose connection and remove users routes because of redundancy

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const methodOverride = require('method-override');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const passport = require('passport');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { mainModule } = require('process');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URL;
 
-var app = express();
+main().catch(err => console.log(err));
+
+async function main()
+{
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +33,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
