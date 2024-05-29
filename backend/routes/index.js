07d2fc2b3412469aa23e251cb0e43488a177f7cf
Batuhan <bthnclk011@gmail.com>
2024-05-29 14:26:38 +0300

    chore(routes): add routes to index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userControllers = require('../controllers/userControllers');
+const postControllers = require('../controllers/postControllers');
+const commentControllers = require('../controllers/commentControllers');
+const utilityControllers = require('../controllers/utilityControllers');
+const authControllers = require('../controllers/authControllers');
+// USERS API REQUEST
+router.get('/users', userControllers.get_users);
+router.post('/users', userControllers.post_users);
+router.get('/users/:userId'.userControllers.get_user);
+router.put('/users/:userId', userControllers.put_user);
+router.delete('/users/:userId', userControllers.delete_user);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// POSTS API REQUEST
+router.get('/posts', postControllers.get_posts);
+router.post('/post-create', postControllers.post);
+router.get('/posts/:postId', postController.get_post_page);
+router.put('/posts/:postId', postController.put_post_page);
+router.delete('/posts/:postId', postController.delete_post_page);
+
+//COMMENTS API REQUEST
+router.get('/posts/:postId/comments', commentControllers.get_comments);
+router.get('/posts/:postId/:commentId', commentControllers.get_comment);
+router.put('/posts/:postId/:commentId', commentControllers.put_comment);
+router.delete('/posts/:postId/:commentId', commentControllers.delete_comment);
+router.post('/posts/:postId/post-comment', commentControllers.post_comment);
+
+//CATEGORIES API REQUEST
+router.get('/categories', utilityControllers.get_categories);
+router.post('/categories', utilityControllers.get_categories);
+router.put('/categories/:categoryId', utilityControllers.put_categories);
+router.delete('/categories/:categoryId', utilityControllers.delete_categories);
+
 
 module.exports = router;
