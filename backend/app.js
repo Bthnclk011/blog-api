a7b0db249d7e8054346e80e6aa18bead57553b96
Batuhan <bthnclk011@gmail.com>
2024-05-30 19:57:08 +0300

    chore(app.js): add authentication logic

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
-const passport = require('passport');
-require('dotenv').config();
 const mongoose = require('mongoose');
-const { mainModule } = require('process');
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const Users = require('./models/Users');
+require('dotenv').config();
 
 const app = express();
 mongoose.set('strictQuery', false);
@@ -28,6 +30,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const opts = {}
+opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET;
+
+passport.use(new JWTStrategy(opts, async(jwt_payload, done) => 
+{
+  try
+  { 
+    const user = Users.findById(jwt_payload.sub);
+    if(user)
+    {
+      return done(null, user)
+    }
+
+    else
+    {
+      return done(null, false)
+    }
+  }
+
+  catch(err)
+  {
+    return done(err, false)
+  }
+      
+}));
+
+app.use(passport.initialize());
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
